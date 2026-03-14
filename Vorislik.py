f27af5e8838f19d84009abf359efbd4415760aac
Muxlisa Rajabboyeva <156774932+muxlissarajabboyeva@users.noreply.github.com>
2026-03-14 07:45:59 +0500

    Refactor Dukon and Gul_dukon classes for clarity

--- Vorislik.py
+++ Vorislik.py
@@ -1,33 +1,38 @@
-# Vorislik va polimorfizm
 class Dukon:
-    def __init__(self,nomi,manzili,turi,telefoni):
-        self.nomi=nomi
-        self.manzili=manzili
-        self.turi=turi
-        self.telefoni=telefoni
-        self.tovarlar=[]
-        
-    def get_info(self) :
+    def __init__(self, nomi, manzili, turi, telefoni):
+        self.nomi = nomi
+        self.manzili = manzili
+        self.turi = turi
+        self.telefoni = telefoni
+        self.tovarlar = []
+
+    def get_info(self):
         return f"{self.nomi} {self.manzili} {self.turi} {self.telefoni}"
-    
-    def add_tovar(self,new):
+
+    def add_tovar(self, new):
         self.tovarlar.append(new)
-    
+
     def get_tovarlar(self):
         return self.tovarlar
-    
+
     def __repr__(self):
         return self.nomi
-    
-class Gul_dukon(Dukon):
-    def __init__(self,nomi,manzili,turi,telefoni,gullar_soni):
-        super.__init__(nomi,manzili,turi,telefoni)
-        self.gullar_soni=gullar_soni
-    
-    def get_info(self):
-        full=super().get_info()
-        full+=self.gullar_soni
-        return full 
 
-obj1=Gul_dukon("Maftuna","Urganch","Gullar","+998933491141")    
-            
+
+# Vorislik va polimorfizm
+class Gul_dukon(Dukon):
+    def __init__(self, nomi, manzili, turi, telefoni, gullar_soni):
+        super().__init__(nomi, manzili, turi, telefoni)
+        self.gullar_soni = gullar_soni
+
+    # Polimorfizm – get_info metodini o‘zgartirish
+    def get_info(self):
+        full = super().get_info()
+        full += " " + str(self.gullar_soni)
+        return full
+
+
+# Ob’ekt yaratish
+obj1 = Gul_dukon("Maftuna", "Urganch", "Gullar", "+998933491141", 120)
+
+print(obj1.get_info())  # Maftuna Urganch Gullar +998933491141 120
