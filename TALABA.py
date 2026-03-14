52146fb170b222ea1adb11b8505e7dcd71f0cc69
Muxlisa Rajabboyeva <156774932+muxlissarajabboyeva@users.noreply.github.com>
2026-03-14 07:40:46 +0500

    Refactor Talaba class and improve methods

--- TALABA.py
+++ TALABA.py
@@ -1,28 +1,35 @@
 class Talaba:
-    number=0
-    def __init__(self,ism,familiya,guruh,shartnoma="Kontrakt"):
-        self.ism=ism
-        self.familiya=familiya
-        self.guruh=guruh
-        self.shartnoma=shartnoma
-        Talaba.number+=1
-        self.__id=Talaba.number
-        
-        def get_id(self):
-            return  self.__id
-    
-        
-    def malumot(self):
-        full=f"Ma'lumot:{self.ism} {self.familiya} {self.guruh}"   
-        return full  
-    def guruh_yangi(self,qiymat):
-        self.guruh=qiymat
-        
-        
-a1=Talaba("Muxlisa","Rajabboyeva","941-22") 
-a2=Talaba("Fotima","Nurmetova","941-22")
-a3=Talaba("Nafosat","Rustambekova","941-22")
+    number = 0
 
-print(a1.ism())   
-a1.guruh_yangi("942-22")        
-print(a1.guruh)
+    def __init__(self, ism, familiya, guruh, shartnoma="Kontrakt"):
+        self.ism = ism
+        self.familiya = familiya
+        self.guruh = guruh
+        self.shartnoma = shartnoma
+        Talaba.number += 1
+        self.__id = Talaba.number
+
+    # ID olish
+    def get_id(self):
+        return self.__id
+
+    # Malumot
+    def malumot(self):
+        return f"Ma'lumot: {self.ism} {self.familiya} {self.guruh}"
+
+    # Guruhni yangilash
+    def guruh_yangi(self, qiymat):
+        self.guruh = qiymat
+
+
+# Ob’ektlar yaratish
+a1 = Talaba("Muxlisa", "Rajabboyeva", "941-22")
+a2 = Talaba("Fotima", "Nurmetova", "941-22")
+a3 = Talaba("Nafosat", "Rustambekova", "941-22")
+
+# Tekshirish
+print(a1.ism)          # Muxlisa
+a1.guruh_yangi("942-22")
+print(a1.guruh)         # 942-22
+print(a1.malumot())     # Ma'lumot: Muxlisa Rajabboyeva 942-22
+print(a1.get_id())      # 1
